a4d5fafc9437c6ebd07255c8f04aab06862d49bb
Administrator_xfl <Administrator_xfl@aliyun.com>
2019-03-17 23:36:06 +0800

    idea整合GitHub测试

--- src/com/xfl/bean/Person.java
+++ src/com/xfl/bean/Person.java
@@ -1,4 +1,24 @@
 package com.xfl.bean;
 
 public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    private String age;
+
 }
