3e1880722f02b8d55e5ade52b3cb7a366f370595
Administrator_xfl <Administrator_xfl@aliyun.com>
2019-03-17 23:40:41 +0800

    idea整合GitHub测试

---
+++ src/com/xfl/bean/Cat.java
@@ -0,0 +1,14 @@
+package com.xfl.bean;
+
+public class Cat {
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    private String height;
+}
