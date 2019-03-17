c8ffce3e572347332313487923b439a2e96d743c
Administrator_xfl <Administrator_xfl@aliyun.com>
2019-03-17 23:42:33 +0800

    idea整合GitHub测试

---
+++ src/com/xfl/bean/Hot.java
@@ -0,0 +1,15 @@
+package com.xfl.bean;
+
+public class Hot {
+
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    private String birth;
+}
